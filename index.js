24c4584ba0522faae1b8ae570499615608d3f65d
Calvin Godfrey <calvinbbcard@gmail.com>
2018-10-14 10:18:04 -0400

    Basic server

---
+++ index.js
@@ -0,0 +1,27 @@
+window.onload = function(){
+  var cameraInput = document.getElementById("photo");
+
+  function getPicture() {
+    var image = cameraInput.files[0];
+    console.log(image);
+
+    var reader = new FileReader();
+
+    if(!image.type.startsWith("image")) {
+      alert("must be image");
+      return;
+    }
+
+    reader.addEventListener("load", function() {
+      document.getElementById("image").src = reader.result;
+      var result = reader.result;
+    }, false);
+
+    reader.readAsDataURL(image);
+
+  }
+
+  cameraInput.addEventListener("change", function(event){
+    getPicture();
+  });
+}
